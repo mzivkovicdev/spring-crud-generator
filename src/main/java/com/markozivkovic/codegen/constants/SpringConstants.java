87c4284fa1ca714fd1c190284d35c3803474384e
zivkovicmarko95 <zivkovic.marko.ftn@gmail.com>
2025-07-29 19:39:46 +0200

    Add writer utils and constants, refactor repository generator

---
+++ src/main/java/com/markozivkovic/codegen/constants/SpringConstants.java
@@ -0,0 +1,14 @@
+package com.markozivkovic.codegen.constants;
+
+public class SpringConstants {
+    
+    private SpringConstants() {
+
+    }
+
+    public static final String SPRING_FRAMEWORK_STEREOTYPE = "org.springframework.stereotype";
+    public static final String SPRING_FRAMEWORK_STEREOTYPE_CONTROLLER = SPRING_FRAMEWORK_STEREOTYPE + ".Controller";
+    public static final String SPRING_FRAMEWORK_STEREOTYPE_SERVICE = SPRING_FRAMEWORK_STEREOTYPE + ".Service";
+    
+
+}
---
+++ src/main/java/com/markozivkovic/codegen/constants/TransactionConstants.java
@@ -0,0 +1,11 @@
+package com.markozivkovic.codegen.constants;
+
+public class TransactionConstants {
+    
+    private TransactionConstants() {
+        
+    }
+    
+    public static final String SPRING_FRAMEWORK_TRANSACTION_ANNOTATION_TRANSACTIONAL = "org.springframework.transaction.annotation.Transactional";
+    public static final String TRANSACTIONAL_ANNOTATION = "@Transactional";
+}
