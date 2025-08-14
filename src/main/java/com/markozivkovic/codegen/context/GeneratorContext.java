780bb4a4a4679aa69d100a0461c3257a8deacd26
zivkovicmarko95 <zivkovic.marko.ftn@gmail.com>
2025-08-14 22:34:14 +0200

    add javadoc for GeneratorContext

--- src/main/java/com/markozivkovic/codegen/context/GeneratorContext.java
+++ src/main/java/com/markozivkovic/codegen/context/GeneratorContext.java
@@ -11,10 +11,22 @@ public class GeneratorContext {
 
     }
 
+    /**
+     * Returns true if the given part has already been generated.
+     * 
+     * @param part the part to check
+     * @return true if the part has already been generated
+     */
     public static boolean isGenerated(final String part) {
         return GENERATED_PARTS.contains(part);
     }
 
+    /**
+     * Marks the given part as generated.
+     * 
+     * @param part the part to mark
+     * @return true if the part was not already generated
+     */
     public static boolean markGenerated(final String part) {
         return GENERATED_PARTS.add(part);
     }
