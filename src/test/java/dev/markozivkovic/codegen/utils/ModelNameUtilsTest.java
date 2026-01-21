93b9d0a9f42c3cf0e5d47844bb7b3873cf6a1dd0
zivkovicmarko95 <zivkovic.marko.ftn@gmail.com>
2026-01-21 16:58:05 +0100

    remove unused import

--- src/test/java/dev/markozivkovic/codegen/utils/ModelNameUtilsTest.java
+++ src/test/java/dev/markozivkovic/codegen/utils/ModelNameUtilsTest.java
@@ -1,7 +1,6 @@
 package dev.markozivkovic.codegen.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
