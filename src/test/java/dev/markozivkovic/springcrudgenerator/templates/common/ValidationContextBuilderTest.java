2d8cfeb6d3e83da25e03f970b304009b40df6c9d
mzivkovicdev <mzivkovic.dev@gmail.com>
2026-02-03 15:24:48 +0100

    remove unused import

--- src/test/java/dev/markozivkovic/springcrudgenerator/templates/common/ValidationContextBuilderTest.java
+++ src/test/java/dev/markozivkovic/springcrudgenerator/templates/common/ValidationContextBuilderTest.java
@@ -2,7 +2,6 @@ package dev.markozivkovic.springcrudgenerator.templates.common;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
