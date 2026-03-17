60e4d70e763fedf673c2d6172960d712d4f25e0c
mzivkovicdev <mzivkovic.dev@gmail.com>
2026-03-17 20:06:55 +0100

    add sort definition

---
+++ src/main/java/dev/markozivkovic/springcrudgenerator/models/SortDefinition.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2025-present Marko Zivkovic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.markozivkovic.springcrudgenerator.models;
+
+import java.util.List;
+import java.util.Objects;
+
+public class SortDefinition {
+    
+    private List<String> allowedFields;
+    private String defaultField;
+    private SortDirection defaultDirection = SortDirection.ASC;
+    private Boolean allowMultiple = Boolean.FALSE;
+
+    public SortDefinition() {
+
+    }
+
+    public SortDefinition(final List<String> allowedFields, final String defaultField,
+            final SortDirection defaultDirection, final Boolean allowMultiple) {
+        this.allowedFields = allowedFields;
+        this.defaultField = defaultField;
+        this.defaultDirection = defaultDirection;
+        this.allowMultiple = allowMultiple;
+    }
+
+    public List<String> getAllowedFields() {
+        return this.allowedFields;
+    }
+
+    public SortDefinition setAllowedFields(final List<String> allowedFields) {
+        this.allowedFields = allowedFields;
+        return this;
+    }
+
+    public String getDefaultField() {
+        return this.defaultField;
+    }
+
+    public SortDefinition setDefaultField(final String defaultField) {
+        this.defaultField = defaultField;
+        return this;
+    }
+
+    public SortDirection getDefaultDirection() {
+        return this.defaultDirection;
+    }
+
+    public SortDefinition setDefaultDirection(final SortDirection defaultDirection) {
+        this.defaultDirection = defaultDirection;
+        return this;
+    }
+
+    public Boolean getAllowMultiple() {
+        return this.allowMultiple;
+    }
+
+    public SortDefinition setAllowMultiple(final Boolean allowMultiple) {
+        this.allowMultiple = allowMultiple;
+        return this;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof SortDefinition)) {
+            return false;
+        }
+        final SortDefinition sortDefinition = (SortDefinition) o;
+        return Objects.equals(allowedFields, sortDefinition.allowedFields) &&
+                Objects.equals(defaultField, sortDefinition.defaultField) &&
+                Objects.equals(defaultDirection, sortDefinition.defaultDirection) &&
+                Objects.equals(allowMultiple, sortDefinition.allowMultiple);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(allowedFields, defaultField, defaultDirection, allowMultiple);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " allowedFields='" + getAllowedFields() + "'" +
+            ", defaultField='" + getDefaultField() + "'" +
+            ", defaultDirection='" + getDefaultDirection() + "'" +
+            ", allowMultiple='" + getAllowMultiple() + "'" +
+            "}";
+    }    
+}
